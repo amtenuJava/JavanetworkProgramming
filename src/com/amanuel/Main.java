07d7928b27198cd7a7f43f3004e4aa50e6171cd5
amanuel <amannov21@gmail.com>
2023-09-15 12:59:30 -0600

    Server connects and sends input and outputs to streams.

---
+++ src/com/amanuel/Main.java
@@ -0,0 +1,21 @@
+package com.amanuel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main {
+    public static void main(String[] args) {
+        try(ServerSocket serverSocket=new ServerSocket(5000)){
+            Socket socket=serverSocket.accept();
+            System.out.println("Client is connected");
+            BufferedReader input=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter output=new PrintWriter(socket.getOutputStream(),true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
